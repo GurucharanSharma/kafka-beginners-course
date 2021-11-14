b690948d6b792557c852b73e8453ebdc177d8fc8
Gurucharan <guru.thecharan@gmail.com>
2021-11-14 19:23:06 +0530

    Java Producer
    Creating Java producers

---
+++ src/main/java/com/programming/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,42 @@
+package com.programming.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // create producer properties
+        Properties properties = new Properties();
+        /*properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());*/
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world");
+
+        // send data
+        // Since this is an async call. The program exits the producer never sends the data.
+        // We have to wait until the message is delivered.
+        producer.send(record);
+
+        // flush data
+        // This waits until the message is sent by the producer.
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
+    }
+}
